74df08c4a9eb370934d7cba44bf07bc102a4bd8a
meghanakodipunjula <51787101+meghanakodipunjula@users.noreply.github.com>
2020-12-30 21:11:25 +0530

    Update Constants.java

--- KeyValueStore/KeyValueStore/src/main/java/constants/Constants.java
+++ KeyValueStore/KeyValueStore/src/main/java/constants/Constants.java
@@ -1,7 +1,7 @@
 package constants;
 
 public class Constants {
-    public static final String defaultDataStoreLoc = "/Users/nandinikodipunzula/Documents/proj";
+    public static final String defaultDataStoreLoc = "/Users/meghanakodipunzula/Documents/proj";
     public static final int MILLISECONDS = 1000;
     public static final int MAX_KEY_LENGTH = 32;
     public static final long MAX_FILE_SIZE = 1024 * 1024 * 1000;
@@ -9,6 +9,8 @@ public class Constants {
     // Messages
     public static final String FAILURE_KEY_LENGTH_EXCEEDED = " Key length exceeded the limit(32chars)";
     public static final String FAILURE_FILE_LENGTH_EXCEEDED = "File length exceeded the limit(1gb)";
+    public static final String FAILURE_VALUE_LENGTH_EXCEEDED = "Value length exceeded the limit(16KB)";
+
 
     public static final String FAILURE_KEY_ALREADY_AVAILABLE = " Operation failed due to key already available";
     public static final String FAILURE_READ = " Read failed due to unknown error, please try again later";
